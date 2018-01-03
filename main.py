dfb6a1bce2b00ef160bbcfe3b25682dbaa42af81
Simon <simon.toivo@telhaug.no>
2018-01-03 22:36:10 +0100

    Display name if that exists

--- main.py
+++ main.py
@@ -8,7 +8,23 @@ class PsOutputPanelCommand(WindowCommand):
         if len(self.panels) == 0:
             self.window.status_message("No panels found")
             return
-        self.window.show_quick_panel(self.panels, self.on_select, idx, 0, self.on_hightlight)
+        self.window.show_quick_panel(
+            self.explucive_panel_names(self.panels),
+            self.on_select,
+            idx,
+            0,
+            self.on_hightlight)
+
+    def explucive_panel_names(self, panels):
+        def name(panel):
+            name = panel[len("output."):]
+            view = self.window.find_output_panel(name)
+            if view.file_name() is None or view.name() == "":
+                return name
+            else:
+                return [name, "{} {}".format(view.file_name() or "", view.name())]
+
+        return [name(p) for p in panels]
 
     def on_select(self, idx):
         if idx == -1:
